2b2d4a10d74cd10e8238c022a1947b9c1353f194
cdrishinski <cdrishinski@gmail.com>
2019-06-20 15:14:53 -0600

    added firebase to app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers'
 
 class App extends Component {
+    componentWillMount() {
+        var firebaseConfig = {
+            apiKey: "AIzaSyAw8dnIPGTLbgZZ0778p1bueHXGz1EwpdA",
+            authDomain: "manager-9f8c1.firebaseapp.com",
+            databaseURL: "https://manager-9f8c1.firebaseio.com",
+            projectId: "manager-9f8c1",
+            storageBucket: "manager-9f8c1.appspot.com",
+            messagingSenderId: "107896274220",
+            appId: "1:107896274220:web:bd3fe924eb922a15"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+    }
+
     render () {
         return (
             <Provider store={createStore(reducers)}>
