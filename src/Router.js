56cd2a821e5ef68f75352a7f34e3a493069e2f20
cdrishinski <cdrishinski@gmail.com>
2019-06-21 23:13:50 -0600

    added form component to handle new employees to be added

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,9 @@
 //defines routes  a user can visit
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
     return (
@@ -15,11 +16,16 @@ const RouterComponent = () => {
                 </Scene>
                 <Scene key="main">
                     <Scene 
+                        rightTitle="Add"
+                        onRight={ () => { Actions.employeeCreate() }}
                         key='employeeList' 
                         component={EmployeeList} 
-                        title='Employees'  
+                        title='Employees'
+                        initial  
                     />
+                    <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
                 </Scene>
+                
                 
             </Scene>
         </Router>
